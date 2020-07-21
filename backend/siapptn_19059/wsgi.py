c38e46ef592712920cbe7ee9733fca56aba128bc
Crowdbotics <team@crowdbotics.com>
2020-07-21 06:24:04 +0000

    Update models

--- backend/siapptn_19059/wsgi.py
+++ backend/siapptn_19059/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siapptn_19059.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siapptn_19059.settings")
 
 application = get_wsgi_application()
